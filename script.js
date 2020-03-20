0f16f0f68adde779619d1052c616713a89338088
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-20 13:08:46 -0700

    append biult articles to the DOM

--- script.js
+++ script.js
@@ -48,21 +48,12 @@ function init() {
      * Builds then updates DOM with the built card elements
      */
     function buildArticleUI(data) {
-        let cards = data.map(cardData => {
+        let articleCards = data.map((cardData, index) => {
             let articleWrapperEl = $('<article>')
 
             // set article classes
             articleWrapperEl.addClass('card p-2')
 
-            //     <article class="card p-2">
-            //     <h5>1. Sample Article</h5>
-            //     <h6>Author Name</h6>
-            //     <p class="card-text">Section: Sports</p>
-            //     <p class="card-text">1972-01-09T05:00:00+0000</p>
-            //     <a href="#" class="card-link">https://www.nytimes.com/sampleurl</a>
-            //   </article>
-
-            // let articleEl = $('<article>')
             let header5El = $('<h5>')
             let header6El = $('<h6>')
             let pEl = $('<p>')
@@ -74,26 +65,19 @@ function init() {
             let pubDate = cardData.pub_date
             let articleLink = cardData.web_url 
 
-            articleWrapperEl.append(header5El.text(title))
+            articleWrapperEl.append(header5El.text(`${index + 1}. ${title}`))
             articleWrapperEl.append(header6El.text(byAuthor))
             articleWrapperEl.append(pEl.text(section).addClass('card-text'))
             articleWrapperEl.append(pEl.text(pubDate).addClass('card-text'))
-            articleWrapperEl.append(linkEl.attr("href", articleLink))
+            articleWrapperEl.append(linkEl.attr("href", articleLink).addClass('card-link').text(articleLink))
 
             return articleWrapperEl
         })
-        console.log('[BUILDARTICLEIO][CARD DATA]', cards)
-        // loop through {data} and build a card for each article
-            // $(cardEl).addClass('class1 class2 ......')
-            // TODO: Need classes that are getting added to each card
-        
-        // Append cardEl to body
-            // TODO: #articles
 
-        
+        $('#articles').append(articleCards)
     }
 
-  doApiQuery('elections', 10, 2010, 2017)
+  doApiQuery('elections', 10, 2010, 2017) 
 
 }
 
