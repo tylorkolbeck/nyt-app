70b8c69b5b1bcbb5515bb7463f5235d56d661e5c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-20 13:35:54 -0700

    set up event listeners on the form buttons

--- script.js
+++ script.js
@@ -3,27 +3,38 @@
 init()
 
 function init() {
+
+
+    // set up eventlisteners
+    $('#search-form').on('click', handleFormSubmit)
+    $('#clear-results').on('click', handleArticleClear)
+
+    function handleArticleClear(event) {
+        event.preventDefault()
+        $("#articles").empty()
+    }
+
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+        let searchTermEl = $('#searchTermEl').val()
+        let numberOfRecords = $('#numberRecords').val()
+        let startYear = $('#startYear').val()
+        let endYear = $('#endYear').val()
+        doApiQuery(searchTermEl, numberOfRecords, parseInt(startYear), parseInt(endYear)) 
+
+    }
+
     let apiKey = 'ydvNHGQto4tLcOSZ4aWQ21b9AxTpwOpm'
 
-
     /**
-     * 
      * @param {string} searchString 
      * @param {num} articleNum 
      * @param {num} startYear 
      * @param {num} endYear
-     * options
-     * {
-     *  begin_date: YYYY,
-     *  end_date: YYYY
-     * 
-     * }
-     * Fetches data from NYT API 
-     * 
      */ 
     function doApiQuery(searchString, articleNum, startYear, endYear) {
         let queryString = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchString}&api-key=${apiKey}`
-
 
         if (startYear) queryString = `${queryString}&begin_date=${startYear}0101`
         if (endYear) queryString = `${queryString}&end_date=${endYear}1230`
@@ -43,7 +54,6 @@ function init() {
     }
 
     /**
-     * 
      * @param {object} data 
      * Builds then updates DOM with the built card elements
      */
@@ -52,7 +62,7 @@ function init() {
             let articleWrapperEl = $('<article>')
 
             // set article classes
-            articleWrapperEl.addClass('card p-2')
+            articleWrapperEl.addClass('card p-2 mb-3')
 
             let header5El = $('<h5>')
             let header6El = $('<h6>')
@@ -77,7 +87,6 @@ function init() {
         $('#articles').append(articleCards)
     }
 
-  doApiQuery('elections', 10, 2010, 2017) 
 
 }
 
