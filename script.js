051c8635dce24ea74cb0983cc460a7f54f0fba58
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-03-20 12:52:30 -0700

    added articles loop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 init()
 
 function init() {
@@ -8,17 +10,31 @@ function init() {
      * 
      * @param {string} searchString 
      * @param {num} articleNum 
+     * @param {num} startYear 
+     * @param {num} endYear
+     * options
+     * {
+     *  begin_date: YYYY,
+     *  end_date: YYYY
+     * 
+     * }
      * Fetches data from NYT API 
      * 
      */ 
-    function doApiQuery(searchString, articleNum) {
+    function doApiQuery(searchString, articleNum, startYear, endYear) {
         let queryString = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchString}&api-key=${apiKey}`
+
+
+        if (startYear) queryString = `${queryString}&begin_date=${startYear}0101`
+        if (endYear) queryString = `${queryString}&end_date=${endYear}1230`
+
 
         $.ajax({
             url: queryString,
             method: 'GET'
         })
             .then(({response} = response) => {
+                console.log(response.docs.slice(0, articleNum))
                 buildArticleUI(response.docs.slice(0, articleNum))
             })
             .catch((err) => {
@@ -33,17 +49,26 @@ function init() {
      */
     function buildArticleUI(data) {
         let cardEl = $('<article>')
+        let cards = data.map(cardData => {
+            let title = cardData.headline.main
+            let byAuthor = cardData.byline.original
+            let section = cardData.section_name
+            let pubDate = cardData.pub_date
+            let articleLink = cardData.web_url
+            
+        })
+        console.log('[BUILDARTICLEIO][CARD DATA]', cards)
         // loop through {data} and build a card for each article
             // $(cardEl).addClass('class1 class2 ......')
             // TODO: Need classes that are getting added to each card
         
         // Append cardEl to body
-            // TODO: Need id of body div to append to
+            // TODO: #articles
 
         
     }
 
-    doApiQuery('elections', 2)
+  doApiQuery('elections', 10, 2010, 2017)
 
 }
 
